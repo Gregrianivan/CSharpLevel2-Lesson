1f91aa06e8d3329e4b1d22bbb69618e68fc63e93
Gregrianivan <ivan.syrovatskij@gmail.com>
2020-02-05 21:57:45 +0700

    lesson6

---
+++ Lesson6/Company/Windows/DepEditWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Company
+{
+    /// <summary>
+    /// Interaction logic for DepEditWindow.xaml
+    /// </summary>
+    public partial class DepEditWindow : Window
+    {
+        uint depid;
+        public DepEditWindow(uint id, string oldName)
+        {
+            InitializeComponent();
+            tblOldName.Text = oldName;
+            depid = id;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainWindow.db.editDep(tboxNewName.Text,depid))
+            {
+                MessageBox.Show("Название отдела изменено!");
+                this.Close();
+            }
+            else
+                MessageBox.Show("Такое название уже используется!");
+        }
+    }
+}
